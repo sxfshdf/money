aa4dac40fbf72885f8f7a4eb34de99a6f5cf956b
斯潇枫 <hzsixiaofeng@highstore.cn>
2020-04-02 17:36:06 +0800

    引入svg，封装icon组件

---
+++ src/components/Icon.vue
@@ -0,0 +1,36 @@
+<template>
+  <svg class="icon">
+    <use :xlink:href="'#' + icon"/>
+  </svg>
+</template>
+
+<script>
+  // let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext)
+  let importAll = (requireContext) => requireContext.keys().forEach(requireContext)
+  try {
+    importAll(require.context('../assets/icons', true, /\.svg$/))
+  } catch (error) {
+    console.log(error)
+  }
+  export default {
+    name: "Icon",
+    props: {
+      icon: {
+        type: String
+      }
+    },
+    created() {
+      console.log(this.icon, 'xxx')
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .icon {
+    width: 1em;
+    height: 1em;
+    vertical-align: -0.15em;
+    fill: currentColor;
+    overflow: hidden;
+  }
+</style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,17 +1,22 @@
 <template>
   <div>
     <router-link to="/money">Money</router-link>
-    <router-link to="/labels">Labels</router-link>
+    <router-link to="/labels">
+      <Icon icon="discount"></Icon>
+      Labels
+    </router-link>
     <router-link to="/statistics">Statistics</router-link>
   </div>
 </template>
 
 <script>
+  import x from '@/assets/icons/discount.svg'
+  console.log(x, 'xx')
   export default {
     name: "Nav"
   }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 
 </style>
